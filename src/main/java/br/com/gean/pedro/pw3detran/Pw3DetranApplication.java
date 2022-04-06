b7e2299d4858949f489335f130828f9b58f83ea0
gean25 <patussigean@gmail.com>
2022-04-06 13:25:39 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/gean/pedro/pw3detran/Pw3DetranApplication.java
@@ -0,0 +1,13 @@
+package br.com.gean.pedro.pw3detran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw3DetranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw3DetranApplication.class, args);
+	}
+
+}
